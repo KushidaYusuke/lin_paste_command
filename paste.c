a258a0f28df39c74d6d5defc1555ec70e27c0cdf
yusuke <kushida.yusuke@formzu.co.jp>
2024-04-09 11:30:51 +0900

    オプションが指定されない場合の処理を実装

--- paste.c
+++ paste.c
@@ -11,8 +11,62 @@ char *dparam = NULL;
 
 
 
+void paste(int file_num, char **file_list, bool *is_file_end_list, FILE **file_list_pointer) {
+  char delim = '\t'; //まずは区切り文字が一つのみの場合を考える
+  bool is_all_file_end = true;//すべてのファイルが終端に達したかどうかを管理
+  int now_index = 0;
+  char *colom_list = malloc(INIT_ALLOC*sizeof(char));
+  int now_alloc_num = INIT_ALLOC;
+  while(1) {
+    for(int i = 0; i < file_num; i++) {
+      if(now_index >= now_alloc_num) colom_list = realloc(colom_list, (i+ADD_ALLOC)*sizeof(char));
+      if(is_file_end_list[i]) {
+        colom_list[now_index] = delim;
+	now_index += 1;
+	continue;
+      }
+      FILE *file = file_list_pointer[i];
+      int c;
+      while(1) {
+	c = fgetc(file);
+	if(c == '\n') {
+	  if(i != file_num-1) {
+	    colom_list[now_index] = delim;
+	    now_index += 1;
+          }
+	  //is_all_file_end = false;
+	  file_list_pointer[i] = file; //中断した時点でのポインタを格納
+	  break;
+	}
+	if(c == EOF) {
+	  is_file_end_list[i] = true;
+	  if(i != file_num - 1) {
+	    colom_list[now_index] = delim;
+	    now_index += 1;
+	  }
+	  break;
+	}
+        is_all_file_end = false;
+	colom_list[now_index] = c;
+	now_index += 1;
+      }
+    }
+    if(is_all_file_end) {
+      return;
+    }
+    for(int i = 0; i < now_index; i++) {
+      putchar(colom_list[i]);
+    }
+    free(colom_list);
+    colom_list = malloc(INIT_ALLOC*sizeof(char));
+    now_index = 0;
+    now_alloc_num = INIT_ALLOC;
+    putchar('\n');
+    is_all_file_end = true;
+  }
+}
 
-void option_d(int file_num, char **file_list, bool *is_file_end_list, FILE **file_list_pointer, int d_param_num) {
+void paste_option_d(int file_num, char **file_list, bool *is_file_end_list, FILE **file_list_pointer, int d_param_num) {
   //char delim = dparam[0]; //まずは区切り文字が一つのみの場合を考える
   bool is_all_file_end = true;//すべてのファイルが終端に達したかどうかを管理
   int now_index = 0;
@@ -98,13 +152,15 @@ int main(int argc, char *argv[]) {
     file_list_pointer[i-optind] = fopen(file_list[i-optind], "r"); 
   }
  
-  int d_param_num = 0;
-  while(dparam[d_param_num] != '\0') {
-    d_param_num += 1;
+  if(dopt) {
+    int d_param_num = 0;
+    while(dparam[d_param_num] != '\0') {
+      d_param_num += 1;
+    }
+    paste_option_d(file_num, file_list, is_file_end_list, file_list_pointer, d_param_num);
   }
-  printf("dparam_numは%d\n", d_param_num); 
-  if(dopt) option_d(file_num, file_list, is_file_end_list, file_list_pointer, d_param_num);
-
+  else paste(file_num, file_list, is_file_end_list, file_list_pointer);
+ 
 
  // for(int i = 0; i < file_num; i++) {
    // printf("%s", file_list[i]);

4653bbb5147b8a2e30e6affc1c70309a16655584
yusuke <kushida.yusuke@formzu.co.jp>
2024-04-11 10:40:07 +0900

    動的にメモリを確保する際のエラー処理を追加

--- paste.c
+++ paste.c
@@ -9,21 +9,42 @@ int dopt = 0;
 int sopt = 0;
 char *dparam = NULL;
 
-//オプションが指定されていない場合
-//引数 file_num: ファイルの個数 
-//is_file_end_list: それぞれのファイルについて、ファイルの終端までたどりついたか判定する変数
-//file_list_pointer: それぞれのファイルへのポインタを格納した配列  
+bool memory_allocate_error = false; //動的配列でのメモリ割り当てが失敗した場合にtrueを返す
+/*
+オプションが指定されていない場合
+引数 
+file_num: ファイルの個数 
+is_file_end_list: それぞれのファイルについて、ファイルの終端までたどりついている場合にtruwを返す配列	
+file_list_pointer: それぞれのファイルへのポインタを格納した配列  
+*/
 void paste(int file_num, bool *is_file_end_list, FILE **file_list_pointer) {
   char delim = '\t'; //デフォルトではタブ区切り	
   bool is_all_file_end = true;//すべてのファイルが終端に達したかどうかを管理
   int now_index = 0;//現在見ている文字が行頭から何番目か
   char *tmp_row = malloc(INIT_ALLOC*sizeof(char)); //読み込んだ文字を一時的に保管しておく
+  if(tmp_row==NULL) {
+    memory_allocate_error = true;
+    return;
+  }
   int now_alloc_num = INIT_ALLOC;//現在割り当てられているメモリの要素数
   
   //すべてのファイルで終端までたどり着くまでループする
   while(1) {
     for(int i = 0; i < file_num; i++) {
-      if(now_index >= now_alloc_num) tmp_row = realloc(tmp_row, (i+ADD_ALLOC)*sizeof(char));
+      //メモリが不足した場合は再割り当て
+      if(now_index >= now_alloc_num) {
+	char *tmp = realloc(tmp_row, (i+ADD_ALLOC)*sizeof(char));
+	if(tmp == NULL) {
+	  memory_allocate_error = true;
+	  free(tmp);
+	  free(tmp_row);
+	  return;
+	}
+	else {
+	  tmp_row = tmp;
+	  now_alloc_num += ADD_ALLOC;
+	}
+      }
       //現在見ているファイルがすでに終端までたどり着いている場合は単に区切り文字を出力	
       if(is_file_end_list[i]) {
         tmp_row[now_index] = delim;
@@ -67,6 +88,10 @@ void paste(int file_num, bool *is_file_end_list, FILE **file_list_pointer) {
     }
     free(tmp_row);
     tmp_row = malloc(INIT_ALLOC*sizeof(char));
+    if(tmp_row == NULL) {
+      memory_allocate_error = true;
+      return;
+    }
     now_index = 0;
     now_alloc_num = INIT_ALLOC;
     putchar('\n');
@@ -80,10 +105,28 @@ void paste_option_d(int file_num, bool *is_file_end_list, FILE **file_list_point
   int now_index = 0;
   int dparam_index = 0; //出力する区切り文字のインデックス 
   char *tmp_row = malloc(INIT_ALLOC*sizeof(char));
+  if(tmp_row == NULL) {
+    memory_allocate_error = true;
+    return;
+  }
+
   int now_alloc_num = INIT_ALLOC;
   while(1) {
     for(int i = 0; i < file_num; i++) {
-      if(now_index >= now_alloc_num) tmp_row = realloc(tmp_row, (i+ADD_ALLOC)*sizeof(char));
+      if(now_index >= now_alloc_num) {
+	char *tmp = realloc(tmp_row, (i+ADD_ALLOC)*sizeof(char));
+	if(tmp == NULL) {
+	  memory_allocate_error = true;
+	  free(tmp);
+	  free(tmp_row);
+	  return;
+	}
+	else {
+	  tmp_row = tmp;
+          now_alloc_num += ADD_ALLOC;
+	}
+      }
+
       if(is_file_end_list[i]) {
 	dparam_index %= d_param_num;
         tmp_row[now_index] = dparam[dparam_index];
@@ -129,6 +172,10 @@ void paste_option_d(int file_num, bool *is_file_end_list, FILE **file_list_point
     }
     free(tmp_row);
     tmp_row = malloc(INIT_ALLOC*sizeof(char));
+    if(tmp_row == NULL) {
+      memory_allocate_error = true;
+      return;
+    }
     now_index = 0;
     dparam_index = 0;
     now_alloc_num = INIT_ALLOC;
@@ -144,11 +191,27 @@ void paste_option_d(int file_num, bool *is_file_end_list, FILE **file_list_point
 void paste_option_s(FILE *file) {
   int now_index = 0;
   char *tmp_row = malloc(INIT_ALLOC*sizeof(char));
+  if(tmp_row == NULL) {
+    memory_allocate_error = true;
+    return;
+  }
   int now_alloc_size = INIT_ALLOC;
   int c;
   while(1) {
-    if(now_index >= now_alloc_size) tmp_row = realloc(tmp_row, (now_index + ADD_ALLOC)*sizeof(char));
-    now_alloc_size += ADD_ALLOC; 
+    if(now_index >= now_alloc_size) {
+      char *tmp = realloc(tmp_row, (now_index + ADD_ALLOC)*sizeof(char));
+      if(tmp == NULL) {
+        free(tmp);
+	free(tmp_row);
+	memory_allocate_error = true;
+	return;
+      }
+      else {
+	tmp_row = tmp;
+        now_alloc_size += ADD_ALLOC;
+      }
+    }
+
     c = fgetc(file);
     if(c == '\n') {
       tmp_row[now_index] = '\t';
@@ -172,14 +235,27 @@ void paste_option_s(FILE *file) {
 void paste_option_s_with_d(FILE *file, int d_param_num) {
   int now_index = 0;
   char *tmp_row = malloc(INIT_ALLOC*sizeof(char));
+  if(tmp_row == NULL) {
+    memory_allocate_error = true;
+    return;
+  }
   int now_alloc_size = INIT_ALLOC;
   int delim_count = 0;
   int c;
   bool is_before_n = false;
   while(1) {
     if(now_index >= now_alloc_size) {
-      tmp_row = realloc(tmp_row, (now_index+ADD_ALLOC)*sizeof(char));
-      now_alloc_size += ADD_ALLOC;
+      char *tmp = realloc(tmp_row, (now_index+ADD_ALLOC)*sizeof(char));
+      if(tmp == NULL) {
+        free(tmp);
+	free(tmp_row);
+	memory_allocate_error = true;
+	return;
+      }
+      else {
+        tmp_row = tmp;
+        now_alloc_size += ADD_ALLOC;
+      }
     }
 
     c = fgetc(file);
@@ -247,6 +323,10 @@ int main(int argc, char *argv[]) {
         }
 	paste_option_s(file);
 	fclose(file);
+	if(memory_allocate_error) {
+	  printf("memory allocation failed\n");
+	  exit(1);
+	}
       }
     }
     //-s,-dオプションがともに指定された場合の処理
@@ -258,11 +338,15 @@ int main(int argc, char *argv[]) {
 	  exit(1);
 	}
         paste_option_s_with_d(file, d_param_num);
-	putchar('\b');
 	fclose(file);
+	if(memory_allocate_error) {
+	  printf("memory allocation failed\n");
+	  exit(1);
+	}
       }
     }
   }
+  //-sオプションが指定されていない場合
   else{
     for(int i = optind; i < argc; i++) {
       is_file_end_list[i-optind] = false;
@@ -281,6 +365,10 @@ int main(int argc, char *argv[]) {
     for(int i = 0; i < file_num; i++) {
       FILE *file = file_list_pointer[i];
       fclose(file);
+    } 
+    if(memory_allocate_error) {
+      printf("memory allocation failed\n");
+      exit(1);
     }
   }
 
